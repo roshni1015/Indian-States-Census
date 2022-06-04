c883753eacb2869858b1e13c47e202ed28978956
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-04 11:35:33 +0530

    [Roshni]-Given the State Census

--- IndianStatesCensusAnalyser/CensusAnalyserTest/UnitTest1.cs
+++ IndianStatesCensusAnalyser/CensusAnalyserTest/UnitTest1.cs
@@ -7,9 +7,12 @@ namespace CensusAnalyserTest
 {
     public class Tests
     {
-        string csvPath = @"C:\Users\Admin\source\c#\Indian-States-Census\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCensusData.csv";
-
+        
         string IndianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+        string IndianStateCensusFilePath = @"C:\Users\Admin\source\c#\Indian-States-Census\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCensusData.csv";
+        string wrongHeaderIndianCensusFilePath = @"C:\Users\Admin\source\c#\Indian-States-Census\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaState.csv";
+
+
         CensusAnalyser censusAnalyser;
         Dictionary<string, CensusDTO> totalRecord;
         Dictionary<string, CensusDTO> stateRecord;
@@ -22,11 +25,27 @@ namespace CensusAnalyserTest
 
         }
 
+        //Use case - 1
+        //Happy Test Case 1.1 : the records are checked
         [Test]
-        public void Test1()
+        public void GivenIndianCensusDataFile_For_ReturnDataFile()
         {
-            totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, csvPath, IndianStateCensusHeaders);
+            totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, IndianStateCensusFilePath, IndianStateCensusHeaders);
             Assert.AreEqual(29, totalRecord.Count);
+        }
+
+        //Sad Test Case 1.2 : to verify if the exception is raised.
+        [Test]
+        public void GivenWrongFilePath_For_ReturnException()
+        {
+            try
+            {
+                totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, wrongHeaderIndianCensusFilePath, IndianStateCensusHeaders);
+            }
+            catch (CensusAnalyserException e)
+            {
+                Assert.AreEqual("File Not Found", e.Message);
+            }
         }
     }
 }
